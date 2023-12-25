269c696608cb97cc46006b2eb6eb014087f2fb77
harisejaz2206 <harisejaz2206@gmail.com>
2023-12-25 09:29:14 +0500

    Cloning an array method 2

--- CloneArray.js
+++ CloneArray.js
@@ -1,5 +1,13 @@
 function clone(array) {
-  return [...array];
+  var newArray = [];
+  array.forEach((element) => {
+    newArray.push(element);
+  });
+  return newArray;
 }
 
-console.log(clone([1, 2, 3, 4]));
+var array1 = [1, 2, 3, 4, 5, 6];
+
+var newArrray1 = clone(array1);
+
+console.log(newArrray1);
