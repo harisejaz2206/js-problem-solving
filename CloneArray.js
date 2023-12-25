a0121f036e69d6126fe9288d35f4a25001ff983e
harisejaz2206 <harisejaz2206@gmail.com>
2023-12-25 09:33:01 +0500

    Cloning an array method 3 (map)

--- CloneArray.js
+++ CloneArray.js
@@ -6,8 +6,18 @@ function clone(array) {
   return newArray;
 }
 
+function cloneUsingMap(array) {
+  var newArray = array.map((e) => {
+    return e;
+  });
+  return newArray;
+}
+
 var array1 = [1, 2, 3, 4, 5, 6];
+var array2 = [7, 8, 9, 10, 11, 12];
 
 var newArrray1 = clone(array1);
+console.log("new array 1", newArrray1);
 
-console.log(newArrray1);
+var newArray2 = cloneUsingMap(array2);
+console.log("new array 2", newArray2);
