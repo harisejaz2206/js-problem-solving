370defe85e926121bba9a0f7da198ebdcd00e0a6
harisejaz2206 <harisejaz2206@gmail.com>
2023-12-28 12:39:14 +0500

    Change to: function which returns first n elements of array

--- function-which-returns-first-n-element-of-array.js
+++ function-which-returns-first-n-element-of-array.js
@@ -1,10 +1,15 @@
 const retrieve = (array, n = 1) => {
   console.log("The array and n are:", array, n);
   let newArray = [];
-  for (let i = 0; i < n; i++) {
-    newArray.push(array[i]);
+  if (n <= array.length) {
+    for (let i = 0; i < n; i++) {
+      newArray.push(array[i]);
+    }
+  } else {
+    console.log("The value of n is greater than the length of the array");
+    return array;
   }
-  return newArray;
+  return 1;
 };
 
-console.log(retrieve([1, 2, 3, 4, 5], 3));
+console.log(retrieve([1, 2, 3, 4, 5], 7));
