4ab654fce133386d790543b08f6b6bada388b8a4
harisejaz2206 <harisejaz2206@gmail.com>
2024-01-29 19:21:33 +0500

    Largest subarray of sum K

---
+++ slidingwindow.largest-subarray-of-sum-k.js
@@ -0,0 +1,35 @@
+const largestSubarray = (array, k) => {
+  // let k represrent  the sum to be achieved
+  let size = array.length;
+  let i = 0;
+  let j = 0;
+  let sum = 0;
+  let largest = -10000;
+
+  while (j < size) {
+    sum += array[j];
+    if (sum < k) {
+      j++;
+    }
+    if (sum === k) {
+      let windowSize = j - i + 1;
+      if (windowSize > largest) {
+        largest = windowSize;
+      }
+      j++;
+    }
+    if (sum > k) {
+      while (sum > k) {
+        sum -= array[i];
+        i++;
+      }
+      j++;
+    }
+  }
+  return largest;
+};
+
+let array = [4, 1, 1, 1, 2, 3, 5];
+let k = 5;
+
+console.log(largestSubarray(array, 5));
