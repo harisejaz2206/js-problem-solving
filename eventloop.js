a182141bd2e244fecc3134342a0c2b283503c4fa
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-12 22:31:45 +0500

    event loop basics

---
+++ eventloop.js
@@ -0,0 +1,9 @@
+console.log(1);
+setTimeout(() => {
+  console.log(2);
+}, 0);
+const promise = new Promise((resolve, reject) => {
+  resolve();
+});
+promise.then(() => console.log(3));
+console.log(4);
