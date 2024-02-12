198cc70a52dc0870a2a32f385b884295a9ce9f7e
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-12 23:36:32 +0500

    insert node logic

--- trees.structure.js
+++ trees.structure.js
@@ -1,5 +1,5 @@
 class Node {
-  constructor(node) {
+  constructor(data) {
     this.data = data;
     this.left = null;
     this.right = null;
@@ -8,13 +8,37 @@ class Node {
 
 class BinaryTree {
   constructor() {
-    this.root = 5;
+    this.root = null;
   }
 
-  logging() {
-    console.log("Tree:", this.root);
+  insert(data) {
+    const newNode = new Node(5);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      this.insertNode(this.root, newNode);
+    }
+  }
+
+  insertNode(parentNode, newNode) {
+    if (newNode.data < parentNode.data) {
+      // new node data is greater than parent node data so we add to right side
+      if (parentNode.left === null) {
+        parentNode.left = newNode;
+      } else {
+        insertNode(parentNode.left, newNode);
+      }
+    } else {
+      // new node data is greater than parent node data so we add to right side
+      if (parentNode.right === null) {
+        parentNode.right = newNode;
+      } else {
+        insertNode(parentNode.right, newNode);
+      }
+    }
   }
 }
 
 const tree = new BinaryTree();
+const node = new Node(6);
 tree.logging();
