3942578b07922e21e440177b72eb875265604ef6
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-13 15:32:06 +0500

    working on tree traversals

--- trees.depthtraversals.js
+++ trees.depthtraversals.js
@@ -11,35 +11,9 @@ class BinaryTree {
     this.root = null;
   }
 
-  insert(data) {
-    const node = new Node(data);
-    if (this.root === null) {
-      this.root = node;
-    } else {
-      this.insertNode(this.root, node);
-    }
-  }
-
-  insertNode(parentNode, newNode) {
-    if (newNode.data < parentNode.data) {
-      // insert on left
-      if (parentNode.left === null) {
-        parentNode.left = newNode;
-      } else {
-        this.insertNode(parentNode.left, newNode);
-      }
-    } else {
-      // insert on right
-      if (parentNode.right === null) {
-        parentNode.right = newNode;
-      } else {
-        this.insertNode(parentNode.right, newNode);
-      }
-    }
-  }
-
   insertIterative(data) {
     const newNode = new Node(data);
+    console.log("inside");
     if (this.root === null) {
       // root is null
       this.root = newNode;
@@ -64,12 +38,26 @@ class BinaryTree {
       }
     }
   }
+
+  // Helper method to perform inorder traversal
+  inorderTraversal(node) {
+    if (node === null) {
+      return;
+    } else {
+      this.inorderTraversal(node.left);
+      console.log(node.data);
+      this.inorderTraversal(node.right);
+    }
+  }
 }
 
 const tree = new BinaryTree();
-tree.insert(5);
-tree.insert(3);
-tree.insert(4);
-tree.insert(2);
-tree.insert(9);
-console.log("Binary Tree:", JSON.stringify(tree, null, 2));
+tree.insertIterative(5);
+tree.insertIterative(3);
+tree.insertIterative(4);
+tree.insertIterative(2);
+tree.insertIterative(9);
+// console.log("Binary Tree:", JSON.stringify(tree, null, 2));
+
+console.log("Inorder traversal:");
+tree.inorderTraversal(tree.root);
