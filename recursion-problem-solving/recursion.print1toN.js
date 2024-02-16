bf5e278f29e8774f84dd1ae8b126b8853f38fd76
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-16 16:54:09 +0500

    recursion | print numbers n to 1

--- recursion-problem-solving/recursion.print1toN.js
+++ recursion-problem-solving/recursion.print1toN.js
@@ -1,12 +1,35 @@
 // Induction-base condition-hypothesis method
 
-function print(n) {
+/*
+Hypothesis step: 
+Signature of the function. 
+print(n) ->  n to 1 laake dega
+print(n-1) -> (n-1) to 1 laake dega
+
+Induction step:
+what we need to do along with the recursive call
+console.log(n)
+ */
+
+// print 1 to N
+function printUpwards(n) {
   if (n == 1) {
     console.log(n);
-    return 1;
+    return;
   }
   print(n - 1);
   console.log(n);
 }
 
-print(7);
+// print N to 1
+function printDownwards(n) {
+  if (n == 1) {
+    console.log(n);
+    return;
+  }
+  console.log(n);
+  print(n - 1);
+}
+
+printUpwards(7);
+printDownwards(7);
