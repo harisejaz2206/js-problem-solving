725b3a8cd9c1015d36b7e5e70a31f714f0196016
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-17 17:07:32 +0500

    working on trees breadth-first search

---
+++ trees-problem-solving/trees.breadth-traversals.js
@@ -0,0 +1,60 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insertIterative(data) {
+    const newNode = new Node(data);
+    if (this.root === null) {
+      // root is null
+      this.root = newNode;
+    } else {
+      let currentParentNode = this.root;
+      while (true) {
+        if (data < currentParentNode.data) {
+          // insert on left
+          if (currentParentNode.left === null) {
+            currentParentNode.left = newNode;
+            break;
+          }
+          currentParentNode = currentParentNode.left;
+        } else {
+          // insert on right
+          if (currentParentNode.right === null) {
+            currentParentNode.right = newNode;
+            break;
+          }
+          currentParentNode = currentParentNode.right;
+        }
+      }
+    }
+  }
+
+  levelOrder(root) {
+    // array operations to use with queue: shift(), push(element)
+    var queue = [];
+    queue.push(root.data);
+
+    while (queue.length > 0) {}
+  }
+}
+
+const tree = new BinaryTree();
+tree.insertIterative(5);
+tree.insertIterative(3);
+tree.insertIterative(4);
+tree.insertIterative(2);
+tree.insertIterative(9);
+tree.insertIterative(1);
+// console.log("Binary Tree:", JSON.stringify(tree, null, 2));
+
+console.log("Level order traversal:");
+tree.levelOrder(tree.root);
