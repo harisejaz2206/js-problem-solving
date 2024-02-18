c911433912374f8147cc7925282c67bc7c9011d8
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-18 18:37:21 +0500

    delete middle element of stack

---
+++ recursion-problem-solving/recursion.delete-mid-elem-of-stack.js
@@ -0,0 +1,25 @@
+function helper(stack, k) {
+  // base condition
+  if (k === 1) {
+    stack.pop();
+    return;
+  }
+  let temp = stack[stack.length - 1];
+  stack.pop();
+  helper(stack, k - 1);
+  stack.push(temp);
+}
+
+function deleteMiddle(stack, size) {
+  if (stack.length === 0) {
+    return;
+  }
+  let k = Math.floor(size / 2 + 1);
+  console.log("k:", k);
+  helper(stack, k);
+}
+
+let stack = [5, 4, 3, 2, 1];
+console.log("stack before deleting middle", stack);
+deleteMiddle(stack, 5);
+console.log("stack before deleting middle", stack);
