52c3b4a71f1913d97c0e3ba53d9280b169860b17
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-20 19:39:09 +0500

    permutation with spaces done

--- recursion-problem-solving/recursion.permutation-with-spaces.js
+++ recursion-problem-solving/recursion.permutation-with-spaces.js
@@ -1,4 +1,25 @@
+function permutationWithSpaces(input, output, size) {
+  if (input.length === size) {
+    output = output.concat(input[0]);
+    input = input.substring(1);
+  }
+
+  if (input.length === 0) {
+    console.log(output);
+    return;
+  }
+
+  let output1 = output;
+  let output2 = output;
+  output1 = output1 + " " + input[0];
+  output2 = output + input[0];
+  input = input.substring(1);
+
+  permutationWithSpaces(input, output1, size);
+  permutationWithSpaces(input, output2, size);
+}
 var input = "abc";
 var output = "";
+var size = input.length;
 
-permutationWithSpaces(input, output);
+permutationWithSpaces(input, output, size);
