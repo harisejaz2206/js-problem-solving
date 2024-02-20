ec7f3443db77e71adff03e99bd53fa1825e44eeb
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-21 01:54:57 +0500

    generate all balanced parentheses with minor mistake - commented line was missing

--- recursion-problem-solving/recursion.generate-all-balanced-parenthesis.js
+++ recursion-problem-solving/recursion.generate-all-balanced-parenthesis.js
@@ -15,7 +15,6 @@ function solve(input, openCount, closeCount, answerArray, initialAnswer) {
   if (openCount === closeCount) {
     initialAnswer += "(";
     openCount--;
-    solve(input, openCount, closeCount, answerArray, initialAnswer);
     return;
   }
   let option1 = initialAnswer + "(";
