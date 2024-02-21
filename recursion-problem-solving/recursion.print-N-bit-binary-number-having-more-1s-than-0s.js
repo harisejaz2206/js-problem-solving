e77789a5704e66266e9adadc5165bee975bfc560
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-21 17:44:31 +0500

    done | print N-bit binary numbers having more 1s than 0s

--- recursion-problem-solving/recursion.print-N-bit-binary-number-having-more-1s-than-0s.js
+++ recursion-problem-solving/recursion.print-N-bit-binary-number-having-more-1s-than-0s.js
@@ -3,24 +3,27 @@ function solve(n, answerArray, onesCount, zeroesCount, initialAnswer) {
     answerArray.push(initialAnswer);
     return;
   }
-  if (onesCount === 0 && zeroesCount === 0) {
-    initialAnswer.push("1");
-    onesCount++;
-    n--;
-    solve(n, answerArray, onesCount, zeroesCount, initialAnswer);
+
+  // Add '1' to the string
+  let answer1 = initialAnswer + "1";
+  solve(n - 1, answerArray, onesCount + 1, zeroesCount, answer1);
+
+  // Add '0' only if there are more or equal '1's than '0's
+  if (onesCount > zeroesCount) {
+    let answer2 = initialAnswer + "0";
+    solve(n - 1, answerArray, onesCount, zeroesCount + 1, answer2);
   }
-  solve(n, answerArray, onesCount + 1, zeroesCount, initialAnswer);
-  solve(n, answerArray, onesCount, zeroesCount + 1, initialAnswer);
 }
 
 function printNBitBinaryNumbers(n, answerArray) {
-  console.log("n, answerArray", n, answerArray);
+  if (n <= 0) return;
   let zeroesCount = 0;
   let onesCount = 1;
   let initialAnswer = "1";
-  solve(n, answerArray, onesCount, zeroesCount, initialAnswer);
+  solve(n - 1, answerArray, onesCount, zeroesCount, initialAnswer);
 }
 
-let input = 3;
+let input = 5;
 let answerArray = [];
 printNBitBinaryNumbers(input, answerArray);
+console.log(answerArray);
