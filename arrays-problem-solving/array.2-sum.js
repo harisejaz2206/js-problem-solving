3b0f5551c7969381472abf75dbbadc2c83263c0a
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-22 19:32:30 +0500

    2-sum optimized

--- arrays-problem-solving/array.2-sum.js
+++ arrays-problem-solving/array.2-sum.js
@@ -12,6 +12,22 @@ function bruteForce(array, target) {
   }
 }
 
+function twoSum(nums, target) {
+  const numMap = {}; // HashMap to store elements and their indices
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (numMap.hasOwnProperty(complement)) {
+      // If complement exists in the hashmap, return indices
+      return [numMap[complement], i];
+    }
+    // Otherwise, store current element and its index in the hashmap
+    numMap[nums[i]] = i;
+  }
+  // If no solution is found, return null or throw an error, depending on requirements
+  return null;
+}
+
 // Example usage:
 const nums = [2, 7, 11, 15];
 const target = 9;
