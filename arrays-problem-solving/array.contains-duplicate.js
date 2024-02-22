6b9047f02ca4201725b14737efd7ab24ab17ecc9
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-22 19:59:58 +0500

    contains duplicate done using hashmap

--- arrays-problem-solving/array.contains-duplicate.js
+++ arrays-problem-solving/array.contains-duplicate.js
@@ -5,4 +5,17 @@ function containsDuplicate(array) {
       ? arrayMap[element]++
       : (arrayMap[element] = 1);
   });
+
+  console.log("arrayMap:", arrayMap);
+
+  for (const element in arrayMap) {
+    if (arrayMap[element] > 1) {
+      return true;
+    }
+    return false;
+  }
 }
+
+var array1 = [1, 3, 4, 2, 6, 7, 4, 3, 1, 5];
+var array2 = [1, 2, 3, 4, 5];
+console.log(containsDuplicate(array2));
