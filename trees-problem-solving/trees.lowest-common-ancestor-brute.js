a5b22e7d69648f8d84dc44811a1908201801795f
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-24 18:38:35 +0500

    lowest common ancestor brute force

--- trees-problem-solving/trees.lowest-common-ancestor-brute.js
+++ trees-problem-solving/trees.lowest-common-ancestor-brute.js
@@ -42,6 +42,22 @@ class BinaryTree {
     if (root === null) {
       return false;
     }
+
+    array.push(root.data);
+
+    if (root.data === value) {
+      return true;
+    }
+
+    if (
+      this.solve(root.left, value, array) ||
+      this.solve(root.right, value, array)
+    ) {
+      return true;
+    }
+
+    array.pop();
+    return false;
   }
 
   findPath(root, value) {
@@ -49,18 +65,39 @@ class BinaryTree {
     if (root === null) {
       return array;
     }
-    solve(root, value, array);
+    this.solve(root, value, array);
+    return array;
   }
 
-  lowestCommonAncestor(root) {}
+  //
+  lowestCommonAncestor(array1, array2) {
+    let size1 = array1.length;
+    let size2 = array2.length;
+    let lowestCommonAncestor = -1;
+
+    let n = Math.abs(size1 - size2);
+
+    for (let i = 0; i < n; i++) {
+      if (array1[i] === array2[i]) {
+        lowestCommonAncestor = array1[i];
+      } else {
+        break;
+      }
+    }
+    console.log("lowest common ancestor:", lowestCommonAncestor);
+  }
 }
 
 const tree = new BinaryTree();
 tree.insertIterative(5);
 tree.insertIterative(3);
 tree.insertIterative(2);
-// tree.insertIterative(1);
+tree.insertIterative(1);
 tree.insertIterative(9);
 // console.log("Binary Tree:", JSON.stringify(tree, null, 2));
 
-console.log("Binary tree balanced: ", tree.checkIfBalanced(tree.root));
+let array1 = tree.findPath(tree.root, 2);
+let array2 = tree.findPath(tree.root, 1);
+console.log("array1:", array1);
+console.log("array2:", array2);
+tree.lowestCommonAncestor(array1, array2);
