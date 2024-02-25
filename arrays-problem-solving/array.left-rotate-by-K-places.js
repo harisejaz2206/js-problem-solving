e5adb070bcde483ff7462cf985c393a331307ab3
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-25 18:48:32 +0500

    left rotate array by K places using loops

--- arrays-problem-solving/array.left-rotate-by-K-places.js
+++ arrays-problem-solving/array.left-rotate-by-K-places.js
@@ -1,1 +1,33 @@
-let array = [1, 2, 3, 4, 5];
+const rotateByKPlaces_usingJS = (array, k) => {
+  let n = array.length;
+  if (n === 0) return array;
+  k = k % n;
+
+  let temp = array.splice(0, k);
+  console.log("Array:", array);
+  array = [...array, ...temp];
+  return array;
+};
+
+const rotateByKPlaces = (array, k) => {
+  let n = array.length;
+  let j = 0;
+  let temp = [];
+  for (let i = 0; i < k; i++) {
+    temp.push(array[i]);
+  }
+
+  for (let i = k; i < array.length; i++) {
+    array[i - k] = array[i];
+  }
+
+  for (let i = n - k; i < array.length; i++) {
+    array[i] = temp[j];
+    j++;
+  }
+  return array;
+};
+
+let array = [1, 2, 3, 4, 5, 6, 7];
+let k = 3;
+console.log(rotateByKPlaces(array, k));
