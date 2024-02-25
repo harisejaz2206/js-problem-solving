265f7db97fb780280b655fef4e4330fd9353c017
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-25 22:12:35 +0500

    Move zeroes to end of array | brute force

--- arrays-problem-solving/array.move-zeroes-to-right.js
+++ arrays-problem-solving/array.move-zeroes-to-right.js
@@ -18,6 +18,13 @@ function moveZeroesMyLogic(array) {
 /*
 To correct this, you can iterate through the array from the end to the beginning. This way, when you 
 splice out an element, it won't affect the indices of elements you haven't processed yet.
+
+-> The time complexity of the provided moveZeroesMyLogicCorrected function is O(n^2), where n is the length of the input array. 
+This is because for each element in the array, potentially all subsequent elements are shifted when a zero is 
+encountered and moved to the end of the array.
+
+-> The space complexity is O(1) because the algorithm operates in-place, meaning it doesn't use any extra space proportional 
+to the size of the input array.
 */
 function moveZeroesMyLogicCorrected(array) {
   let n = array.length;
@@ -28,8 +35,27 @@ function moveZeroesMyLogicCorrected(array) {
       array.push(temp);
     }
   }
-  console.log(array);
+  console.log("my method", array);
+}
+
+function moveZeroesMyLogicBrute(array) {
+  let temp = [];
+  let zeroCount = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== 0) {
+      temp.push(array[i]);
+    } else {
+      zeroCount++;
+    }
+  }
+
+  for (let i = 0; i <= zeroCount; i++) {
+    temp.push(0);
+  }
+  console.log("brute", temp);
+  return temp;
 }
 
 let array = [1, 3, 4, 0, 4, 6, 0, 0, 1, 3, 0, 3];
 moveZeroesMyLogicCorrected(array);
+moveZeroesMyLogicBrute(array);
