1e43bc25de9dceb442becac166035303b8f26f10
harisejaz2206 <harisejaz2206@gmail.com>
2024-02-25 19:18:36 +0500

    left rotate array by K places best solution my logic

--- arrays-problem-solving/array.left-rotate-by-K-places.js
+++ arrays-problem-solving/array.left-rotate-by-K-places.js
@@ -9,7 +9,7 @@ const rotateByKPlaces_usingJS = (array, k) => {
   return array;
 };
 
-const rotateByKPlacesBetter = (array, k) => {
+const rotateByKPlacesBrute = (array, k) => {
   let n = array.length;
   let j = 0;
   let temp = [];
@@ -28,22 +28,46 @@ const rotateByKPlacesBetter = (array, k) => {
   return array;
 };
 
-const rotateByKPlacesOptimal = (array, k) => {
+function rotateByKPlacesOptimal(array, k) {
   let n = array.length;
-  let temp = [];
-  for (let i = 0; i < k; i++) {
-    temp.push(array[i]);
+  let i = 0;
+  let ptr1 = 0;
+  let ptr2 = k - 1;
+  while (i < k && ptr1 < ptr2) {
+    let temp = array[ptr1];
+    array[ptr1] = array[ptr2];
+    array[ptr2] = temp;
+    ptr1++;
+    ptr2--;
+    i++;
   }
 
-  for (let i = k; i < array.length; i++) {
-    array[i - k] = array[i];
+  let j = k;
+  let ptr3 = k;
+  let ptr4 = array.length - 1;
+  while (j < n && ptr3 < ptr4) {
+    let temp = array[ptr3];
+    array[ptr3] = array[ptr4];
+    array[ptr4] = temp;
+    ptr3++;
+    ptr4--;
+    j++;
   }
 
-  for (let i = n - k; i < array.length; i++) {
-    array[i] = temp[i - (n - k)];
+  ptr1 = 0;
+  ptr4 = array.length - 1;
+  i = 0;
+  while (i < n && ptr1 < ptr4) {
+    let temp = array[ptr1];
+    array[ptr1] = array[ptr4];
+    array[ptr4] = temp;
+    ptr1++;
+    ptr4--;
+    i++;
   }
-  return array;
-};
+
+  console.log(array);
+}
 
 let array = [1, 2, 3, 4, 5, 6, 7];
 let k = 3;
