16fc14d97c46d659df59ccdf79ce713c25c87a93
harisejaz2206 <harisejaz2206@gmail.com>
2024-08-19 22:31:25 +0500

    Pikes Soft problems isAnagram

--- basic-problem-solving/basic.is-anagram-without-sorting-practice.js
+++ basic-problem-solving/basic.is-anagram-without-sorting-practice.js
@@ -14,8 +14,8 @@ const isAnagram = (string1, string2) => {
   const occurences1 = occ(string1);
   const occurences2 = occ(string2);
 
-  //  for...in: Iterates over the keys of an object.
-  //  for...of: Iterates over the values of an iterable object.
+  //  for...in: Iterates over the keys of an object!
+  //  for...of: Iterates over the values of an iterable object!
 
   for (const char in occurences1) {
     if (occurences1[char] !== occurences2[char]) {
